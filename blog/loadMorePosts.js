4e692e2012e24f49933601b6b9503b0a9c7f374f
Tony Gaeta <tgaeta@users.noreply.github.com>
2024-06-17 11:36:22 -0400

    (chore) use es6 syntax, remove semi.

--- blog/loadMorePosts.js
+++ blog/loadMorePosts.js
@@ -8,55 +8,49 @@
  *
  */
 
-document.addEventListener('DOMContentLoaded', function () {
-  const loadMoreLink = document.getElementById('loadMorePosts');
+document.addEventListener('DOMContentLoaded', () => {
+  const loadMoreLink = document.getElementById('loadMorePosts')
 
   if (loadMoreLink) {
-    loadMoreLink.addEventListener('click', function (event) {
-      event.preventDefault();
+    loadMoreLink.addEventListener('click', (event) => {
+      event.preventDefault()
 
-      const nextPage = this.getAttribute('href');
+      const nextPage = loadMoreLink.getAttribute('href')
 
       fetch(nextPage)
         .then((response) => response.text())
         .then((data) => {
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(data, 'text/html');
-          const newPostsContainer = doc.querySelector('#blogPosts');
-          const newNextPageLink = doc.querySelector('#loadMorePosts');
+          const parser = new DOMParser()
+          const doc = parser.parseFromString(data, 'text/html')
+          const newPostsContainer = doc.querySelector('#blogPosts')
+          const newNextPageLink = doc.querySelector('#loadMorePosts')
 
           // Check if newPostsContainer exists
           if (newPostsContainer) {
-            const newPosts = newPostsContainer.innerHTML;
+            const newPosts = newPostsContainer.innerHTML
 
             // Append new posts
-            document
-              .getElementById('blogPosts')
-              .insertAdjacentHTML('beforeend', newPosts);
+            document.getElementById('blogPosts').insertAdjacentHTML('beforeend', newPosts)
           } else {
-            throw new Error('New posts container not found.');
+            throw new Error('New posts container not found.')
           }
 
           // Update the URL using history.pushState
-          history.pushState(
-            null,
-            '',
-            nextPage.endsWith('/') ? nextPage : nextPage + '/'
-          );
+          history.pushState(null, '', nextPage.endsWith('/') ? nextPage : nextPage + '/')
 
           // Update next page link or remove it if no more pages
           if (newNextPageLink) {
-            this.setAttribute('href', newNextPageLink.getAttribute('href'));
+            loadMoreLink.setAttribute('href', newNextPageLink.getAttribute('href'))
           } else {
-            this.remove();
+            loadMoreLink.remove()
           }
         })
-        .catch((error) => console.error('Error loading more posts:', error));
-    });
+        .catch((error) => console.error('Error loading more posts:', error))
+    })
   }
 
-  // Treat back/forward navigation as a page reload to fetch the correct page w/ 10 posts
-  window.addEventListener('popstate', function () {
-    window.location = window.location.href;
-  });
-});
+  // Treat back/forward navigation as a page reload to fetch the correct page
+  window.addEventListener('popstate', () => {
+    window.location = window.location.href
+  })
+})
